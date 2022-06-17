eb3d53d5c3e3562775401e68baabb7e9fd650df5
An <65813540+umegan@users.noreply.github.com>
2022-06-17 17:16:24 +0900

    add move_to_mark

--- example_code/AutoParking.py
+++ example_code/AutoParking.py
@@ -175,7 +175,25 @@ class AutoParking:
 
     # 駐車マークまで移動
     def move_to_mark(self, path_x, path_y, turned_theta):
-        pass
+        count=1
+		while(count!=0):
+			if(count==1):
+				turn_right(turned_theta)
+				if(can_move()==True):
+					count+=1
+
+				elif(count==2):
+					move_forward(path_x)
+					if(can_move()==True):
+						count+=1
+				elif(count==3):
+					turnleft(90)
+					if(can_move()==True):
+						count+=1            
+				elif(count==4):
+					move_forward(path_y)
+					count==0
+		return True
 
     def end_process(self):
         GPIO.cleanup()
