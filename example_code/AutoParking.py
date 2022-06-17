879705d453b520fdb71580648cc5cb2dc116924e
An <gemu.nguyenan@gmail.com>
2022-06-17 16:41:35 +0900

    fix def face_to_mark

--- example_code/AutoParking.py
+++ example_code/AutoParking.py
@@ -131,17 +131,17 @@ class AutoParking:
     # 駐車マークに真っ直ぐ向いていく
     def face_to_mark(self, coordinate_x):
         d = 10
-	count = 0
-	if 300 < coordinate_x < 340:
-	    stop()
-	    turned_theta = count*d
-	    return True, turned_theta
-	elif coordinate_x < 300:
-	    turnleft(d)
-	    count++
-	elif coordinate_x > 340:
-	    turnright(d)
-	    count++
+        count = 0
+        if 300 < coordinate_x < 340:
+            stop()
+            turned_theta = count*d
+            return True, turned_theta
+        elif coordinate_x < 300:
+            turn_left(d)
+            count+= count
+        elif coordinate_x > 340:
+            turn_right(d)
+            count+= count
      
 
     # 駐車マークまでの距離を取る
